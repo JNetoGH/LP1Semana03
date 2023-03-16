ff982dc9220759c967c20c77d1047a49755829b9
JNeto <joaomendes15@gmail.com>
2023-03-16 20:50:20 +0000

    project det

---
+++ ChangeString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChangeString {
+    class Program {
+        
+        static void Main(string[] args) {
+            
+            Console.Write("insert a string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine(str);
+            
+            Console.Write("insert a char to be removed from the string: ");
+            char c = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(c);
+        }
+        
+    }
+}
